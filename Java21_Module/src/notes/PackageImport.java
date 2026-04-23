fc85fd6a4826fcbb9eb1d34bec569286cdc36b9c
sassygirl23 <ricabuenafe21@gmail.com>
2026-04-23 16:14:42 +0800

    Package Import

---
+++ Java21_Module/src/notes/PackageImport.java
@@ -0,0 +1,20 @@
+package notes;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class PackageImport {
+
+    public static void main(String[] args) {
+
+        List<String> names = new ArrayList<>();
+
+        names.add("Rica");
+        names.add("Chochaii");
+        names.add("Sasha");
+
+        for (String name : names) {
+            System.out.println(name);
+        }
+    }
+}	
