101840bd1c47c821f067adc80ffd35aa085ad59c
sassygirl23 <ricabuenafe21@gmail.com>
2026-04-23 14:28:30 +0800

    First/Initial Commit

---
+++ Java21_Module/src/dataType/primitive.java
@@ -0,0 +1,17 @@
+package dataType;
+
+public class primitive {
+	public static void main(String[] args) {
+		int x = 10;
+		double y = 2.5;
+		char c = 'R';
+		boolean flag = true;
+		
+		System.out.print(x);
+		System.out.print(y);
+		System.out.print(c);
+		System.out.print(flag);
+		
+	}
+
+}
---
+++ Java21_Module/src/notes/basicFundamentals.java
@@ -0,0 +1,11 @@
+package notes;
+
+public class basicFundamentals {
+
+	/*
+	 * Java BAsics
+	 * main() entry point
+	 * varaiables must be always initialized
+	 * 
+	 */
+}
