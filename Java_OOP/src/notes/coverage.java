a1faf5c33437009f174eb588f9cb290b42382bd0
sassygirl23 <ricabuenafe21@gmail.com>
2026-04-24 17:34:11 +0800

    OOP

---
+++ Java_OOP/src/notes/coverage.java
@@ -0,0 +1,15 @@
+package notes;
+
+public class coverage {
+//ang hirap mo mahalin programming :<
+    /*
+     * Chapter 5 Summary
+     * - A class can contain fields, methods, constructors, and nested types.
+     * - An object is an instance of a class.
+     * - Object references store addresses, not the actual object itself.
+     * - this refers to the current object.
+     * - Scope can be instance, static, local, or block level.
+     * - Objects become eligible for garbage collection when no reachable references remain.
+     * - //may idadag dag pa hoooo
+     */
+}
