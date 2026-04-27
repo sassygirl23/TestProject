044875edd9f0c4bff98e45f649d3f477d0b0d6ea
sassygirl23 <ricabuenafe21@gmail.com>
2026-04-27 13:23:49 +0800

    Output check

---
+++ Enthuware_Java21/src/Test1/TestClass.java
@@ -0,0 +1,16 @@
+package Test1;
+//output check #42
+public class TestClass{
+
+	   static char ch;
+	   static float f;
+	   static boolean bool;
+
+	   public static void main(String[] args){
+	      System.out.print(f);
+	      System.out.print(" ");
+	      System.out.print(ch);
+	      System.out.print(" ");
+	      System.out.print(bool);
+	   }
+	}
