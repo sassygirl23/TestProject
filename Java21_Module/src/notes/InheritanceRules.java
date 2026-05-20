0de430fb4c40c4bede4b4956a05f24c803f2fd22
sassygirl23 <ricabuenafe21@gmail.com>
2026-05-20 22:19:56 +0800

    Inheritance

---
+++ Java21_Module/src/notes/InheritanceRules.java
@@ -0,0 +1,46 @@
+package notes;
+
+public class InheritanceRules {
+
+    /*
+      INHERITANCE RULES
+     
+     * Extends
+      - ginagamit para mag-inherit from parent class
+     
+     * Super
+      - tawag sa parent constructor
+      - automatic inserted kung walang explicit constructor call
+     
+     * Super.field
+      - access parent field
+     
+     * Super.method()
+      - call parent method
+     
+     * OVERRIDING
+ 
+     * Overriding:
+      - same method name
+      - same parameters
+      - child class gives new behavior
+     
+     * POLYMORPHISM
+ 
+     * Parent reference can point to child object.
+     
+     * Example:
+      Animal a = new Dog();
+     
+     * Instance methods:
+      - object type ang sinusunod
+     
+     * Static methods:
+      - reference type ang sinusunod
+     
+     * ABSTRACT CLASS
+      - cannot instantiate directly
+      - can have abstract methods
+      - can have concrete methods
+     */
+}
---
+++ Java_OOP/src/inheritance/Constructor.java
@@ -0,0 +1,28 @@
+package inheritance;
+
+class Parent {
+
+    Parent() {
+        // Parent constructor unang tatawagin
+        System.out.println("Parent siya");
+    }
+}
+
+class Child extends Parent {
+
+    Child() {
+        // Bago ito mag-run, automatic munang tatawagin ang super()
+        System.out.println("Child ako");
+    }
+}
+
+public class Constructor {
+
+	public static void main(String[] args) {
+		
+		 // Kapag gumawa ng Child object, tatawagin muna ang Parent constructor
+        Child child = new Child();
+
+	}
+
+}
