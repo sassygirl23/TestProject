b42da9237035ee58c592ffc4eb6c622334377dce
sassygirl23 <ricabuenafe21@gmail.com>
2026-06-10 22:02:51 +0800

    streams

---
+++ Lambdas_streams/src/streams/Count.java
@@ -0,0 +1,21 @@
+package streams;
+
+import java.util.List;
+
+public class Count {
+
+    public static void main(String[] args) {
+
+        long count =
+                List.of(1,2,3,4,5)
+
+                        .stream()
+
+                        .count();
+
+        System.out.println(count);
+
+//count() counts elements
+
+    }
+}
