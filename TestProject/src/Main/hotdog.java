97e95b88e39e42de762cbdc234b5321459b0d4f7
sassygirl23 <ricabuenafe21@gmail.com>
2026-07-16 09:51:30 +0800

    tttt

--- TestProject/src/Main/hotdog.java
+++ TestProject/src/Main/hotdog.java
@@ -1,8 +1,5 @@
-package Main;
 
-
-
-
-
-
- 
+// so kung kaya nila, why not diko kaya
+// and after this thing happen i will focus on everything and live my life to the fullest 
+// in God's perfect will and God's perfect time.
+// I can do this in any way possible and everything will work with your guidance.
