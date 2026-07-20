a255c6cdccc394d7eb02e7f6cc819086d4acff4d
sassygirl23 <ricabuenafe21@gmail.com>
2026-07-20 15:55:35 +0800

    Arithmnetic

--- enums/src/enums/Arithmetic.java
+++ enums/src/enums/Arithmetic.java
@@ -1,7 +1,31 @@
 package enums;
 
 public class Arithmetic {
+
+	    // Function na nag-a-add ng kahit ilang integers
+	    static int sum(int... numbers) {
+
+	        int total = 0;
+
+	        // Inuulit ang bawat number at idinadagdag sa total
+	        for (int num : numbers) {
+	            total += num;
+	        }
+
+	        // Ibinabalik ang kabuuang sum
+	        return total;
+	    }
+
+	    public static void main(String[] args) {
+
+	        // Puwedeng magpasa ng kahit ilang integers
+	        int answer = sum(10, 20, 30, 40, 50);
+
+	        System.out.println("Total: " + answer);
+	    }
+	
 	
 	
 
 }
+                                                    
