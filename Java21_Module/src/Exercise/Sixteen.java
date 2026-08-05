299e72cc2df2858823e7562ff464974c5a1b6bec
sassygirl23 <ricabuenafe21@gmail.com>
2026-08-05 15:29:47 +0800

    Exercise #4

--- Java21_Module/src/Exercise/Sixteen.java
+++ Java21_Module/src/Exercise/Sixteen.java
@@ -41,6 +41,30 @@ public class Sixteen {
 
         return filteredImages;
     }
+ // Exercise #4
+    public static void main(String[] args) {
+
+        List<Image> images = new ArrayList<>();
+
+        images.add(new Image(50, 50));
+        images.add(new Image(120, 90));
+        images.add(new Image(80, 80));
+        images.add(new Image(200, 150));
+        images.add(new Image(90, 95));
+
+        // i f'filter yung  images that are smaller than 100 x 100
+        List<Image> smallImages = filterImages(
+                images,
+                image -> image.getWidth() < 100 && image.getHeight() < 100
+        );
+
+        System.out.println("Images smaller than 100 x 100:");
+
+        for (Image image : smallImages) {
+            System.out.println(image);
+        }
+    }
+
 }
 
 
